f04ce6c591524b291e164bc726dfd47767216ba2
kose-uk45 <kosebilaliuk45@gmail.com>
2023-11-15 22:36:34 +0200

    fix(app runtime): downgrade app runtime version

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Provider } from '@dhis2/app-runtime';
 import Home from './components/Home';
 import { API_BASE_URL, API_VERSION } from './config/api';
 import './App.css';
-
 const appConfig = {
   baseUrl: API_BASE_URL,
   apiVersion: API_VERSION,
